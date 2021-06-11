613e43f4acd7df1b981085291d17673c5589951c
malithk911 <78416064+malithk911@users.noreply.github.com>
2021-06-11 11:43:50 +0530

    Count plot added

--- graphs.py
+++ graphs.py
@@ -1,8 +1,15 @@
 import pandas as pd
 import seaborn as sns
+from matplotlib import pyplot as plt
 
 
 def plot_dataset(dataset_type):
     data = pd.read_csv(f"data/ICDS_{dataset_type}_Oversampled_Dataset.csv")
     p = sns.pairplot(data, hue='app_status', kind="kde")
     p.savefig(f"plots/pairplot_{dataset_type}.png")
+
+
+def count_plot(dataset_type):
+    data = pd.read_csv(f"data/ICDS_{dataset_type}_Oversampled_Dataset.csv")
+    sns.countplot(x="app_status", data=data)
+    plt.show()
