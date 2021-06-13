84c0624c3c535ec64da6b95fc1edfc56b98ac05a
CharinduLiyanage <charinduliyanage@yahoo.com>
2021-06-13 23:48:25 +0530

    final submission

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 from KNN import *
-from naive_bayes import *
+from dataset_handling import *
 
+# Making the test_data.csv to numeric.
+create_Numeric("test")
+
+# Making the test_data.csv to numeric.
+create_Numeric("train")
+
+# Creating test and train data split (1:9).
+create_test_train()
+
+# Create oversample datasets (ROS/ADASYN).
+create_oversample()
+
+# Create undersample dataset (RENN).
+create_undersample()
+
+# Running KNN.
 run_KNN("CRAP_ROS")
+
+# Creating submition file.
+final_KNN()
