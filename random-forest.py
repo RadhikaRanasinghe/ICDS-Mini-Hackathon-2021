1a67145bcc7a4e294d0fcd982cfc9df19e5ed5a0
CharinduLiyanage <charinduliyanage@yahoo.com>
2021-06-13 18:02:35 +0530

    RF Updated

--- random-forest.py
+++ random-forest.py
@@ -2,33 +2,53 @@
 
 import pickle
 
-import numpy as np
-import pandas as pd
-from sklearn import metrics
+import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, plot_confusion_matrix
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
+
+import dataset_handling
 
 
 def run_random_forest():
     # Loading the dataset with a Pandas and the returned data frame is caught by data variable
-    data_frame = pd.read_csv("data/CRAP _ROS_Oversampled_Dataset.csv")
-    # cols_to_drop = ['ID']
-    # data_frame = data_frame.drop(cols_to_drop, axis=1)
+    x, y = dataset_handling.preprocessing_columns("CRAP_ROS")
 
-    # Creating 'x' and 'y'
-    x = data_frame.values
-    x = np.delete(x, 8, axis=1)
-    y = data_frame['app_status'].values
+    # initialize(x, y)
 
     highest_accuracy_save(x, y)
 
+    writing()
+
+
+def initialize(x, y):
+    x_test, y_test = dataset_handling.preprocessing_columns("CRAP_test")
+
+    # Splitting the data into testing data and training data with the testing size of 0.3
+    x_train, __, y_train, ___ = train_test_split(x, y, test_size=0.3)
+
+    # The Random Forest Classifier Model is assigned to the model variable
+    model = RandomForestClassifier()
+
+    # The training data from the data split is taken for fitting into the model to train
+    model.fit(x_train, y_train)
+
+    # The accuracy score of the model
+    acc = model.score(x_test, y_test)
+
+    pickle.dump(model, open("models/Random_Forest/Random_Forest_Best_Model.pickle", "wb"))
+    # Saving the training and testing data
+    data = {'x_train': x_train, 'x_test': x_test, 'y_train': y_train, 'y_test': y_test}
+    pickle.dump(data, open("models/Random_Forest/Random_Forest_Best_Data.pickle", "wb"))
+    print("Initial saved", acc)
+
 
 def highest_accuracy_save(x, y):
+    x_test, y_test = dataset_handling.preprocessing_columns("CRAP_test")
+
     for _ in range(10000):
         # Splitting the data into testing data and training data with the testing size of 0.3
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+        x_train, __, y_train, ___ = train_test_split(x, y, test_size=0.3)
 
         # The Random Forest Classifier Model is assigned to the model variable
         model = RandomForestClassifier()
@@ -53,25 +73,15 @@ def highest_accuracy_save(x, y):
         # Updating bestData and bestModel if the new accuracy is better.
         if acc > best_acc:
             print("best saved", acc)
-            pickle.dump(model, open(
-                "models/Random_Forest/Random_Forest_Best_Model.pickle",
-                "wb"))
+            pickle.dump(model, open("models/Random_Forest/Random_Forest_Best_Model.pickle", "wb"))
             # Saving the training and testing data
             data = {'x_train': x_train, 'x_test': x_test, 'y_train': y_train, 'y_test': y_test}
-            pickle.dump(data, open(
-                "models/Random_Forest/Random_Forest_Best_Data.pickle",
-                "wb"))
+            pickle.dump(data, open("models/Random_Forest/Random_Forest_Best_Data.pickle", "wb"))
 
 
 def writing():
-    loaded_best_data = pickle.load(
-        open(
-            "models/Random_Forest/Random_Forest_Best_Data.pickle",
-            "rb"))
-    loaded_best_model = pickle.load(
-        open(
-            "models/Random_Forest/Random_Forest_Best_Model.pickle",
-            "rb"))
+    loaded_best_data = pickle.load(open("models/Random_Forest/Random_Forest_Best_Data.pickle", "rb"))
+    loaded_best_model = pickle.load(open("models/Random_Forest/Random_Forest_Best_Model.pickle", "rb"))
 
     # Running predictions using  test data
     predictions = loaded_best_model.predict(loaded_best_data['x_test'])
@@ -96,4 +106,4 @@ def writing():
         plt.savefig(f"plots/{title}.png".replace(" ", "_").replace(",", ""))
 
 
-writing()
+run_random_forest()
